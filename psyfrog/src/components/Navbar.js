337fde73e3e9924ce38f218a9f49cd564f2c8296
NhojDev <johnduypham@gmail.com>
2024-05-14 13:21:32 -0700

    Navbar

---
+++ psyfrog/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+
+    <>
+        <nav className = "navbar">
+            <div className = "navbar-container">
+                <Link to ="/" className = "navbar-logo">
+                    EXAMPLE
+                </Link>
+            </div>
+        </nav>
+    </>
+  )
+}
